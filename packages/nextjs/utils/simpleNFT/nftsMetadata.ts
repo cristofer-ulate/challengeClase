c000a788db55196f56a4f1562b067c2d286504f6
Cristofer Ulate Bolanos <CUBolanos@nationalindemnity.com>
2025-08-06 20:08:12 -0600

    Metadata NFT agregada

--- packages/nextjs/utils/simpleNFT/nftsMetadata.ts
+++ packages/nextjs/utils/simpleNFT/nftsMetadata.ts
@@ -1,124 +1,28 @@
 const nftsMetadata = [
   {
-    description: "It's actually a bison?",
+    description: "Is it a cute dog?",
     external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/buffalo.jpg",
-    name: "Buffalo",
+    image: "https://scarlet-worrying-echidna-187.mypinata.cloud/ipfs/bafkreiczp4bfnsp4pgzqdr57dfc3wl6ajswa2gnhbo2surdsvnwj4i27ii",
+    name: "Dog",
     attributes: [
       {
-        trait_type: "BackgroundColor",
-        value: "green",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 42,
+        name: "Jack",
+        owner: "Hackathon team",
       },
     ],
   },
   {
-    description: "What is it so worried about?",
+    description: "The best hackathon team",
     external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/zebra.jpg",
-    name: "Zebra",
+    image: "https://scarlet-worrying-echidna-187.mypinata.cloud/ipfs/bafybeifts7bbbskfouyatyff5oncknlbye3c7lyq2wctzdxqrirrxhmqb4",
+    name: "Hackathon Team",
     attributes: [
       {
-        trait_type: "BackgroundColor",
-        value: "blue",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 38,
-      },
+        name: "Hackathon Team",
+        members: "3",
+      }
     ],
-  },
-  {
-    description: "What a horn!",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/rhino.jpg",
-    name: "Rhino",
-    attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "pink",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 22,
-      },
-    ],
-  },
-  {
-    description: "Is that an underbyte?",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/fish.jpg",
-    name: "Fish",
-    attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "blue",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 15,
-      },
-    ],
-  },
-  {
-    description: "So delicate.",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/flamingo.jpg",
-    name: "Flamingo",
-    attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "black",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 6,
-      },
-    ],
-  },
-  {
-    description: "Raaaar!",
-    external_url: "https://austingriffith.com/portfolio/paintings/", // <-- this can link to a page for the specific file too
-    image: "https://austingriffith.com/images/paintings/godzilla.jpg",
-    name: "Godzilla",
-    attributes: [
-      {
-        trait_type: "BackgroundColor",
-        value: "orange",
-      },
-      {
-        trait_type: "Eyes",
-        value: "googly",
-      },
-      {
-        trait_type: "Stamina",
-        value: 99,
-      },
-    ],
-  },
+  }
 ];
 
 export type NFTMetaData = (typeof nftsMetadata)[number];
